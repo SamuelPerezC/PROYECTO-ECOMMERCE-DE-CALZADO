e459a7f663437bc2ee33cb982f6fba2ac1b7eaf9
david970873737 <jesusreyescuellar7@gmail.com>
2026-07-17 20:13:09 -0500

    Delete PROYECTO-ECOMMERCE-DE-CALZADO-main/PROYECTO-ECOMMERCE-DE-CALZADO-main directory

--- PROYECTO-ECOMMERCE-DE-CALZADO-main/PROYECTO-ECOMMERCE-DE-CALZADO-main/PROYECTO-ECOMMERCE-DE-CALZADO/JavaScript/controladores/IndexControlador.js
+++
@@ -1,8 +0,0 @@
-import ApiModelo from "../Servicios/ApiModelo.js";
-
-const api = new ApiModelo();
-
-const usuario = api.obtenerUsuarioActivo();
-
-const nombre = document.querySelector("#nombreUsuario")
-
